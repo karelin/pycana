5f6cc06bda726043ca7dcd360be753808630582c
el-sonido-q <el-sonido-q@f6cf5304-ea63-4e20-8523-c47af1ec3e5c>
2009-08-04 04:12:21 +0000

    cardinal relations

--- pycana/code_analyzer.py
+++ pycana/code_analyzer.py
@@ -1,5 +1,9 @@
 from inspect import *
+from pygraphviz import AGraph
 from collections import defaultdict
+from itertools import chain
+
+from relations import *
 
 def is_container(var):
     return isinstance(var, list) or isinstance(var, dict) or isinstance(var, set)
@@ -24,44 +28,40 @@ class CodeAnalyzer(object):
             if self._belongs_to_module(var):
                 module_vars.append(var)
 
-        instance_aggregation_relations= defaultdict(set)
+        aggregation_relations= defaultdict(set)
         for var in module_vars:
-            self._get_instance_aggregate_relations(var, module_vars, instance_aggregation_relations)
-
-        class_aggregation_relations= self._build_class_relations(instance_aggregation_relations)
+            self._get_aggregation_relations(var, module_vars, aggregation_relations)
 
         if exceptions is not None:
             for klass in exceptions:
-                if klass in class_aggregation_relations: class_aggregation_relations.pop(klass)
+                if klass in aggregation_relations: aggregation_relations.pop(klass)
 
-                for other_klass, related in class_aggregation_relations.iteritems():
-                    class_aggregation_relations[other_klass]= set((yet_another_klass, attrname) 
-                                        for (yet_another_klass, attrname) in related 
-                                            if not yet_another_klass == klass)
+                for other_klass, related in aggregation_relations.iteritems():
+                    aggregation_relations[other_klass]= set(r for r in related if not r.object2 == klass)
         
 
-        all_classes= set(class_aggregation_relations.keys())
-        for related in class_aggregation_relations.itervalues():
-            all_classes.update(i[0] for i in related)
+        all_classes= set(aggregation_relations.keys())
+        for related in aggregation_relations.itervalues():
+            all_classes.update(i.object2 for i in related)
 
+        relations= aggregation_relations
         inheritance_relations= self._build_inheritance_relations(all_classes)
+        for klass, related in inheritance_relations.iteritems():
+            relations[klass].update(related)
 
-        return inheritance_relations, class_aggregation_relations                                 
+        return relations
 
-    def _build_class_relations(self, aggregation_relations):
-        ans= defaultdict(set)
-        for k, v in aggregation_relations.iteritems():
-            ans[k.__class__].update((e[0].__class__, e[1]) for e in v)
-        return ans            
-    
     def _build_inheritance_relations(self, all_classes):
         inheritance_relations= defaultdict(list)
  
         new_classes= set()
         for n1 in all_classes:
-            for super_n1 in n1.mro()[1:-1]:
-                if super_n1 not in all_classes: new_classes.add(super_n1)
-                inheritance_relations[n1].append(super_n1)
+            super_n1= n1.mro()[1]
+            if super_n1 == object: continue
+            if super_n1 not in all_classes: 
+                new_classes.add(super_n1)
+            relation= InheritanceRelation(n1, super_n1)
+            inheritance_relations[n1].append(relation)
 
         all_classes.update(new_classes) 
 
@@ -69,7 +69,10 @@ class CodeAnalyzer(object):
             for n2 in all_classes:
                 if n1 == n2: continue
                 if issubclass(n1, n2):
-                    inheritance_relations[n1].append(n2)
+                    for relation in inheritance_relations[n1]:
+                        if issubclass(n2, relation.object2): break
+                    else:
+                        inheritance_relations[n1].append(InheritanceRelation(n1, n2))
 
         return inheritance_relations
 
@@ -77,53 +80,58 @@ class CodeAnalyzer(object):
         var_module= getmodule(var.__class__)
         return var_module.__name__.startswith(self.base_module.__name__)
         
-    def _get_instance_aggregate_relations(self, module_var, module_vars, aggregation_relations):
+    def _get_aggregation_relations(self, module_var, module_vars, aggregation_relations):
         assert module_var in module_vars
-        aggregation_relations[module_var]
+        aggregation_relations[module_var.__class__]
 
         for attrname, attrvalue in getmembers(module_var):
             if not self._is_member_interesting(attrname): continue
             if self._belongs_to_module(attrvalue):
                 if attrvalue not in module_vars:
                     module_vars.append(attrvalue)
-                    self._get_instance_aggregate_relations(attrvalue, module_vars, aggregation_relations)
-                aggregation_relations[module_var].add((attrvalue, attrname))
+                    self._get_aggregation_relations(attrvalue, module_vars, aggregation_relations)
+
+                relation= AggregationRelation(module_var.__class__, attrvalue.__class__, attrname, is_multiple=False)
+                aggregation_relations[module_var.__class__].add(relation)
 
             if is_container(attrvalue):
-                container_module_variables= self._container_relations(attrvalue, module_vars, aggregation_relations)
-                aggregation_relations[module_var].update((cmv, attrname) for cmv in container_module_variables)
+                container_module_vars= self._container_relations(attrvalue, module_vars, aggregation_relations)
+                for container_module_var in container_module_vars:
+                    relation= AggregationRelation(module_var.__class__, container_module_var.__class__, attrname, is_multiple=True)
+                    aggregation_relations[module_var.__class__].add(relation)
 
     def _container_relations(self, container, module_vars, aggregation_relations):
-        container_module_variables= []
+        container_module_vars= []
         for iterable in itercontainer(container):
             for e in iterable:
                 if self._belongs_to_module(e):
-                    container_module_variables.append(e)
+                    container_module_vars.append(e)
                     if not e in module_vars: module_vars.append(e)
-                    self._get_instance_aggregate_relations(e, module_vars, aggregation_relations)
+                    self._get_aggregation_relations(e, module_vars, aggregation_relations)
                 elif is_container(e):
-                    container_module_variables.extend(self._container_relations(e, module_vars, aggregation_relations))
-        return container_module_variables                    
+                    container_module_vars.extend(self._container_relations(e, module_vars, aggregation_relations))
+        return container_module_vars                    
 
-    def draw_relations(self, aggregation_relations, inheritance_relations, fname):
-        from pygraphviz import AGraph
+    def draw_relations(self, relations, fname):
+        def get_node_name(n):
+            return n.__name__
 
         g= AGraph(directed=True)
-        for n1, related in aggregation_relations.iteritems():
-            for n2, attrname in related:
-                g.add_edge(n1, n2)
-                e= g.get_edge(n1, n2)
-                e.attr['arrowhead']= 'odiamond'
-                e.attr['label']= attrname
-        
-        for n1, subclasses in inheritance_relations.iteritems():
-            for n2 in subclasses:
-                g.add_edge(n1, n2)
-                e= g.get_edge(n1, n2)
-                e.attr['arrowhead']= 'empty'
+        for n in relations:
+            n_name= get_node_name(n)
+            g.add_node(n_name)
+
+        for relation in chain(*relations.itervalues()):
+            n1_name= get_node_name(relation.object1)
+            n2_name= get_node_name(relation.object2)
+            g.add_edge(n1_name, n2_name)
+
+            e= g.get_edge(n1_name, n2_name)
+            relation.set_edge_attributes(e)
 
         for n in g.nodes():
             n.attr['shape']= 'box'
+
         g.draw(fname, prog='dot', args='-Grankdir=TB')
         
     def _is_member_interesting(self, attrname):
