29135677a3a6ba9285f6d2dab017063c30baf2b5
el-sonido-q <el-sonido-q@f6cf5304-ea63-4e20-8523-c47af1ec3e5c>
2010-06-22 16:40:18 +0000

    multi module

--- pycana/code_analyzer.py
+++ pycana/code_analyzer.py
@@ -18,47 +18,34 @@ def itercontainer(c):
         return ([e] for e in c)
 
 class CodeAnalyzer(object):
-    def __init__(self, base_module):
-        self.base_module= base_module
+    def __init__(self, *base_modules):
+        self.base_modules= base_modules
 
     def analyze(self, exceptions=None):
         variables= currentframe(1).f_locals
-        module_classes= set()
         module_vars= []
         for varname, var in variables.iteritems():
             if self._belongs_to_module(var):
-                module_classes.add(var.__class__)
                 module_vars.append(var)
-            elif is_container(var):
-                for e in itercontainer(var):
-                    for k in e:
-                        if self._belongs_to_module(k):
-                            module_classes.add(k.__class__)
-                            module_vars.append(k)
 
         aggregation_relations= defaultdict(set)
         for var in module_vars:
-            self._get_aggregation_relations(var, module_classes, aggregation_relations)
-
-
-        all_classes= set(klass for klass in aggregation_relations.keys())
-        for related in aggregation_relations.itervalues():
-            all_classes.update(i.object2 for i in related)
-
-        inheritance_relations= self._build_inheritance_relations(all_classes)
+            self._get_aggregation_relations(var, module_vars, aggregation_relations)
 
         if exceptions is not None:
             for klass in exceptions:
                 if klass in aggregation_relations: aggregation_relations.pop(klass)
-                if klass in inheritance_relations: inheritance_relations.pop(klass)
 
                 for other_klass, related in aggregation_relations.iteritems():
                     aggregation_relations[other_klass]= set(r for r in related if not r.object2 == klass)
-
-                for other_klass, related in inheritance_relations.iteritems():
-                    inheritance_relations[other_klass]= set(r for r in related if not r.object2 == klass)
         
+
+        all_classes= set(aggregation_relations.keys())
+        for related in aggregation_relations.itervalues():
+            all_classes.update(i.object2 for i in related)
+
         relations= aggregation_relations
+        inheritance_relations= self._build_inheritance_relations(all_classes)
         for klass, related in inheritance_relations.iteritems():
             relations[klass].update(related)
 
@@ -87,51 +74,63 @@ class CodeAnalyzer(object):
                         if issubclass(n2, relation.object2): break
                     else:
                         inheritance_relations[n1].append(InheritanceRelation(n1, n2))
+        
+        for n1 in all_classes:
+            to_remove= []
+            for r1 in inheritance_relations.get(n1, []):
+                n2= r1.object2
+                for r2 in inheritance_relations.get(n2, []):
+                    n3= r2.object2
+                    for r in inheritance_relations[n1]:
+                        if r.object2 == n3: to_remove.append(r)
 
+            for r in to_remove:
+                inheritance_relations[n1].remove(r)
+
+                
         return inheritance_relations
 
     def _belongs_to_module(self, var):
         var_module= getmodule(var.__class__)
-        return var_module.__name__.startswith(self.base_module.__name__)
+        return any(var_module.__name__.startswith(base_module.__name__) for base_module in self.base_modules)
         
-    def _get_aggregation_relations(self, module_var, module_classes, aggregation_relations):
-        assert module_var.__class__ in module_classes
+    def _get_aggregation_relations(self, module_var, module_vars, aggregation_relations):
+        assert module_var in module_vars
         aggregation_relations[module_var.__class__]
 
         for attrname, attrvalue in getmembers(module_var):
             if not self._is_member_interesting(attrname): continue
             if self._belongs_to_module(attrvalue):
-                if attrvalue.__class__ not in module_classes:
-                    module_classes.add(attrvalue.__class__)
-                    self._get_aggregation_relations(attrvalue, module_classes, aggregation_relations)
+                if attrvalue not in module_vars:
+                    module_vars.append(attrvalue)
+                    self._get_aggregation_relations(attrvalue, module_vars, aggregation_relations)
 
                 relation= AggregationRelation(module_var.__class__, attrvalue.__class__, attrname, is_multiple=False)
                 aggregation_relations[module_var.__class__].add(relation)
 
             if is_container(attrvalue):
-                container_module_classes= self._container_relations(attrvalue, module_classes, aggregation_relations)
-                for container_module_class in container_module_classes:
-                    relation= AggregationRelation(module_var.__class__, container_module_class, attrname, is_multiple=True)
+                container_module_vars= self._container_relations(attrvalue, module_vars, aggregation_relations)
+                for container_module_var in container_module_vars:
+                    relation= AggregationRelation(module_var.__class__, container_module_var.__class__, attrname, is_multiple=True)
                     aggregation_relations[module_var.__class__].add(relation)
 
-    def _container_relations(self, container, module_classes, aggregation_relations):
-        container_module_classes= set()
+    def _container_relations(self, container, module_vars, aggregation_relations):
+        container_module_vars= []
         for iterable in itercontainer(container):
             for e in iterable:
                 if self._belongs_to_module(e):
-                    container_module_classes.add(e.__class__)
-                    if not e.__class__ in module_classes: 
-                        module_classes.add(e.__class__)
-                        self._get_aggregation_relations(e, module_classes, aggregation_relations)
+                    container_module_vars.append(e)
+                    if not e in module_vars: module_vars.append(e)
+                    self._get_aggregation_relations(e, module_vars, aggregation_relations)
                 elif is_container(e):
-                    container_module_classes.update(self._container_relations(e, module_classes, aggregation_relations))
-        return container_module_classes                    
+                    container_module_vars.extend(self._container_relations(e, module_vars, aggregation_relations))
+        return container_module_vars                    
 
     def draw_relations(self, relations, fname):
         def get_node_name(n):
             return n.__name__
 
-        g= AGraph(directed=True, strict=False)
+        g= AGraph(directed=True)
         for n in relations:
             n_name= get_node_name(n)
             g.add_node(n_name)
